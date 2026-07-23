081333605be65b593d12655a440b0f5aae9fbe8f
Krish Rajput <krish1135.becse24@chitkara.edu.in>
2026-07-23 11:44:26 +0530

    Time: 15 ms (41.88%) | Memory: 49.4 MB (28.57%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,3 +1,46 @@
+// class Solution {
+//     class pair{
+//         int row;
+//         int col;
+//         pair(int r,int c){
+//             this.row=r;
+//             this.col=c;
+//         }
+
+//     }
+//     public int[][] updateMatrix(int[][] mat) {
+//         int rows=mat.length;
+//         int cols=mat[0].length;
+//         int[][] ans=new int[rows][cols];
+//         Queue<pair> q=new LinkedList<>();
+//         for(int i=0;i<rows;i++){
+//             for(int j=0;j<cols;j++){
+//                 if(mat[i][j]==0){
+//                     ans[i][j]=0;
+//                     q.add(new pair(i,j));
+//                 } else{
+//                     ans[i][j]=-1;
+//                 }
+//             }
+//         }
+//         int[] dRow = {-1, 1, 0, 0};
+//         int[] dCol = {0, 0, -1, 1};
+
+//         while(!q.isEmpty()){
+//             pair front=q.poll();
+//             int row=front.row,col=front.col;
+//             for(int i=0;i<4;i++){
+//                 int newRow=row+dRow[i];
+//                 int newCol=col+dCol[i];
+//                 if(newRow >=0 && newRow < rows && newCol >= 0 && newCol < cols && ans[newRow][newCol]==-1){
+//                     ans[newRow][newCol]=ans[row][col]+1;
+//                     q.add(new pair(newRow,newCol));
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// }
 class Solution {
     class pair{
         int row;
@@ -23,19 +66,25 @@ class Solution {
                 }
             }
         }
-        int[] dRow = {-1, 1, 0, 0};
-        int[] dCol = {0, 0, -1, 1};
 
         while(!q.isEmpty()){
             pair front=q.poll();
             int row=front.row,col=front.col;
-            for(int i=0;i<4;i++){
-                int newRow=row+dRow[i];
-                int newCol=col+dCol[i];
-                if(newRow >=0 && newRow < rows && newCol >= 0 && newCol < cols && ans[newRow][newCol]==-1){
-                    ans[newRow][newCol]=ans[row][col]+1;
-                    q.add(new pair(newRow,newCol));
-                }
+            if(row-1 >= 0 && ans[row-1][col] == -1){
+                ans[row-1][col]=ans[row][col]+1;
+                q.add(new pair(row-1,col));
+            }
+            if(row+1 < rows && ans[row+1][col] == -1){
+                ans[row+1][col]=ans[row][col]+1;
+                q.add(new pair(row+1,col));
+            }
+            if(col-1 >= 0 && ans[row][col-1] == -1){
+                ans[row][col-1]=ans[row][col]+1;
+                q.add(new pair(row,col-1));
+            }
+            if(col+1 < cols && ans[row][col+1] == -1){
+                ans[row][col+1]=ans[row][col]+1;
+                q.add(new pair(row,col+1));
             }
         }
         return ans;
