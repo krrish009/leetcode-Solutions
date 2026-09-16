8a33b771411e258122470b3e2cca4bdfe77f5d09
Krish Rajput <krish1135.becse24@chitkara.edu.in>
2026-09-16 14:40:37 +0530

    Time: 2 ms (98.99%) | Memory: 47.2 MB (35.39%) - LeetSync

---
+++ 2398-check-if-matrix-is-x-matrix/check-if-matrix-is-x-matrix.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                if(i == j || i == cols - 1 - j){
+                    if(grid[i][j] == 0) return false;
+                } else{
+                    if(grid[i][j] != 0) return false;
+                }
+            }
+        }
+        return true;
+    }
+}
