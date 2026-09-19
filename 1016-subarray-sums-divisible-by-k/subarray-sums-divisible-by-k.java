12252f6eb6ec65a21bdd3a48b7cf62873958f728
Krish Rajput <krish1135.becse24@chitkara.edu.in>
2026-09-19 15:33:35 +0530

    Time: 25 ms (55.57%) | Memory: 51.4 MB (73.77%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -8,16 +8,21 @@ class Solution {
         // HashMap to store: <Remainder, Frequency>
         HashMap<Integer, Integer> map = new HashMap<>();
         
+        // Base case: A remainder of 0 has occurred once initially
         map.put(0, 1);
         
         for (int num : nums) {
             currSum += num;
-        
+            
+            // Crucial: Handle negative remainders properly in Java
             int remainder = (currSum % k + k) % k;
-    
+            
+            // If this remainder has been seen before, add its frequency
             if (map.containsKey(remainder)) {
                 count += map.get(remainder);
             }
+            
+            // Track the current remainder in history
             map.put(remainder, map.getOrDefault(remainder, 0) + 1);
         }
         
