44e1a17bca7f8d0a4398fa3b2b09ecbe26b2ac14
Krish Rajput <krish1135.becse24@chitkara.edu.in>
2026-09-26 19:26:48 +0530

    Time: 1 ms (99.91%) | Memory: 43.5 MB (94.85%) - LeetSync

---
+++ 3447-clear-digits/clear-digits.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(Character.isDigit(ch)){
+                if (sb.length() > 0) {
+                    sb.deleteCharAt(sb.length() - 1);
+                }
+            } else {
+                sb.append(ch);
+            }
+        }
+        return sb.toString();
+    }
+}
